99ad9bbea6cdcd9d9df8c979d14abdd33d92e1ff
Lossme8 <46249735+Lossme8@users.noreply.github.com>
2020-02-20 09:51:16 +0200

    Add files via upload

---
+++ Working with part of a list/dimensions.py
@@ -0,0 +1,16 @@
+# tuples
+
+dimensions = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# looping through all values in a tuple
+print("Original dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+# writing over a tuple
+
+dimensions = (400, 100)
+print("\nModified dimensions: ")
+for dimension in dimensions:
+    print(dimension)
---
+++ Working with part of a list/foods.py
@@ -0,0 +1,9 @@
+my_foods = ['pizza', 'lamb', 'carrot cake']
+friends_foods = my_foods[:]
+my_foods.append('wings')
+friends_foods.append('ice cream')
+print("My favourates foods are:")
+print(my_foods)
+
+print("\nMy friend's favourite foods are: ")
+print(friends_foods)
