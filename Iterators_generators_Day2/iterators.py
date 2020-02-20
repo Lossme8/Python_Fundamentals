6a1f72c143da950054ab3b96dcde3654c67dcc6a
Lossme8 <46249735+Lossme8@users.noreply.github.com>
2020-02-20 09:49:43 +0200

    Add files via upload

---
+++ Iterators_generators_Day2/iterators.py
@@ -0,0 +1,21 @@
+ for element in [1 , 2, 3]:
+    print(element)
+
+print("=" * 32)
+for item in (1, 2, 3):
+    print(item)
+
+print("=" * 32)
+for key in {'one':1, 'two':2}:
+    print(key)
+
+print("=" * 32)
+for char in "123":
+    print(char)
+
+print("=" * 32)
+for line in open("myfile.txt"):
+    print(line,end='')
+
+print("=" * 32)
+s ='abc'
