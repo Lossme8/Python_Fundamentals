92cc37d4a6bad430b4c399cf40529036a391cb5e
Lossme8 <46249735+Lossme8@users.noreply.github.com>
2020-02-20 09:51:01 +0200

    Add files via upload

---
+++ Working with list/animals.py
@@ -0,0 +1,6 @@
+animals = [ 'leopards', 'elephants', 'lions']
+for animal in animals:
+    print(animal.title())
+    print(" kruger national park has " + animal.title() + " , they are part of the ecosystem."
+          )
+    print(animal.title() + ", form part of the big five animals.")
---
+++ Working with list/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['chicken' ,'steak', 'feta' ]
+for pizza in pizzas:
+    print(pizza.title())
+    print("I enjoy eating " + pizza.title() + " pizza"'.\n' )
+print(pizza.title() + " has been  my favourite meal since my childhood.\nanother type of pizza I love"
+                      " \nis steak and feta.\nI so like pizza.")
